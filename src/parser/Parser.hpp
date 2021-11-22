be45c778eaeb022f3d47e619391219125bb05bca
sgah <sgah@42.student.fr>
2021-11-22 16:35:06 +0100

    creation of parsing class and main fonction

---
+++ src/parser/Parser.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Parser.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sgah <sgah@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/22 15:32:31 by sgah              #+#    #+#             */
+/*   Updated: 2021/11/22 16:28:07 by sgah             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PARSER_H
+# define PARSER_H
+
+# include "webserv.hpp"
+
+class Parser {
+
+	public:
+
+		Parser(void);
+
+		Parser(Parser const & src);
+		~Parser(void);
+
+		Parser&			operator=(const Parser &rhs);
+
+		void			config(const char *file);
+
+
+	private:
+
+		stringVector	_configfile;
+};
+
+
+#endif
