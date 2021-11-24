5c8655daeb6bfb4af8b59433a4fc9fdc4933a5b7
sherchryst <sherchryst@icloud.com>
2021-11-24 06:11:01 +0100

    add everything that define a server

--- src/config/Config.cpp
+++ src/config/Config.cpp
@@ -6,7 +6,7 @@
 /*   By: sgah <sgah@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 16:16:28 by sgah              #+#    #+#             */
-/*   Updated: 2021/11/24 00:01:40 by sgah             ###   ########.fr       */
+/*   Updated: 2021/11/24 05:51:52 by sgah             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,93 @@ Config::Config(void) {}
 
 Config::Config(Config const & src)
 {
-	(void)src;
+	*this = src;
 }
 
 Config::~Config(void) {}
+
+Config &		Config::operator=(Config const &src)
+{
+
+	_network = src._network;
+	_root = src._root;
+	_server_name = src._server_name;
+	_error_page = src._error_page;
+	_client_body_buffer_size = src._client_body_buffer_size;
+	_cgi_param = src._cgi_param;
+	_cgi_pass = src._cgi_pass;
+	_location = src._location;
+	_allowed_methods = src._allowed_methods;
+	_autoindex = src._autoindex;
+	_index = src._index;
+	_alias = src._alias;
+	_alias_set = src._alias_set;
+
+	return *this;
+}
+
+void		Config::addNetwork(t_network net)
+{
+	_network.push_back(net);
+}
+
+void		Config::addServerName(stringVector Name)
+{
+	_server_name = Name;
+}
+
+void		Config::addRoot(std::string root)
+{
+	_root = root;
+}
+
+void		Config::addClientBodyBufferSize(int buffer)
+{
+	_client_body_buffer_size = buffer;
+}
+
+void		Config::addCgiParam(stringMap cgiParam)
+{
+	_cgi_param = cgiParam;
+}
+
+void		Config::addCgiPass(std::string cgiPass)
+{
+	_cgi_pass = cgiPass;
+}
+
+void		Config::addLocation(serverMap location)
+{
+	_location = location;
+}
+
+void		Config::addAllowedMethods(stringVector methods)
+{
+	_allowed_methods = methods;
+}
+
+void		Config::addIndex(stringVector index)
+{
+	_index = index;
+}
+
+void		Config::addAutoIndex(bool autoIndex)
+{
+	_autoindex = autoIndex;
+}
+
+void		Config::addAlias(std::string alias)
+{
+	_alias = alias;
+	_alias_set = true;
+}
 
 netVector		Config::getNetwork(void)
 {
 	return (_network);
 }
 
-stringVector	Config::getServerNAme(void)
+stringVector	Config::getServerName(void)
 {
 	return (_server_name);
 }
@@ -76,3 +152,8 @@ std::string		Config::getAlias(void)
 	return (_alias);
 }
 
+std::ostream	&operator<<(std::ostream &out, const Config &server)
+{
+	out << "root: " << server._root << std::endl;
+	return (out);
+}
--- src/config/Config.hpp
+++ src/config/Config.hpp
@@ -6,7 +6,7 @@
 /*   By: sgah <sgah@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 16:16:28 by sgah              #+#    #+#             */
-/*   Updated: 2021/11/23 23:51:13 by sgah             ###   ########.fr       */
+/*   Updated: 2021/11/24 05:59:04 by sgah             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,22 +24,24 @@ class Config {
 		Config(Config const & src);
 		~Config(void);
 
+		friend std::ostream	&operator<<(std::ostream &out, const Config &server);
 		Config &	operator=(const Config &rhs);
 
-		void			addNetwork(stringVector args);
-		void			addServerNAme(stringVector args);
-		void			addRoot(stringVector args);
-		void			addClientBodyBufferSize(stringVector args);
-		void			addCgiParam(stringVector args);
-		void			addCgiPass(stringVector args);
+		void			addNetwork(t_network network);
+		void			addServerName(stringVector serverName);
+		void			addRoot(std::string root);
+		void			addClientBodyBufferSize(int buffer);
+		void			addCgiParam(stringMap cgiParam);
+		void			addCgiPass(std::string cgiPass);
 		void			addLocation(serverMap locations);
-		void			addAllowedMethods(stringVector args);
-		void			addIndex(stringVector args);
-		void			addAutoIndex(stringVector args);
-		void			addAlias(stringVector args);
+		void			addAllowedMethods(stringVector methods);
+		void			addIndex(stringVector index);
+		void			addAutoIndex(bool autoIndex);
+		void			addAlias(std::string alias);
+		void			addErrorPage(stringMap pages);
 
 		netVector		getNetwork(void);
-		stringVector	getServerNAme(void);
+		stringVector	getServerName(void);
 		std::string		getRoot(void);
 		int				getClientBodyBufferSize(void);
 		stringMap		getCgiParam(void);
@@ -49,11 +51,13 @@ class Config {
 		stringVector	getIndex(void);
 		bool			getAutoIndex(void);
 		std::string		getAlias(void);
+		stringMap		getErrorPage(void);
 
 	private:
 		netVector		_network;
 		std::string		_root;
 		stringVector	_server_name;
+		stringMap		_error_page;
 		int				_client_body_buffer_size;
 		stringMap		_cgi_param;
 		std::string		_cgi_pass;
@@ -63,7 +67,6 @@ class Config {
 		bool			_autoindex;
 		std::string		_alias;
 		bool			_alias_set;
-
 };
 
 
