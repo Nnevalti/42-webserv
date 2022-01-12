6af13504f97485d6937b8205f18c5c6dde847c4f
Nnevalti <deschamps.valentin.1998@gmail.com>
2022-01-12 17:06:09 +0100

    CGI File upload

--- pages/index/form.php
+++ pages/index/form.php
@@ -1,9 +1,28 @@
 <?php
 
-echo "try";
+echo "try\n";
 if (!empty($_POST))
 {
-	echo 'Bonjour ' . $_POST["text1"] . htmlspecialchars($_POST["text2"]) . '!';
+	echo 'Bonjour ' . $_POST["text1"] . htmlspecialchars($_POST["text2"]) . "!\n";
+	// UPLOAD
+	$uploaddir = '/mnt/nfs/homes/vdescham/Documents/42-webserv/pages/index/upload/';
+	$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+	$tmp_name = $_FILES['userfile']['tmp_name'];
+	echo $tmp_name;
+	echo '<pre>';
+	if (move_uploaded_file($tmp_name, $uploadfile)) {
+	    echo "Le fichier est valide, et a été téléchargé
+	           avec succès. Voici plus d'informations :\n";
+	} else {
+	    echo "Attaque potentielle par téléchargement de fichiers.
+	          Voici plus d'informations :\n";
+	}
+
+	echo 'Voici quelques informations de débogage :';
+	print_r($_FILES);
+
+	echo '</pre>';
+
 }
 else
 {
