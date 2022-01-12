6c3ab862a86171ef3618fdb10e91469f5376b811
Nnevalti <deschamps.valentin.1998@gmail.com>
2022-01-12 14:56:23 +0100

    Fixed garbage value for host when invalid method

--- src/parser/ParseRequest.cpp
+++ src/parser/ParseRequest.cpp
@@ -55,7 +55,7 @@ void			Parser::parseFirstLine(Request &request)
 	if ((end = first.find_first_of(' ')) == std::string::npos)
 	{
 		request.setRet(400);
-		std::cerr << RED << "Error parsing request: missing first line element" << SET << std::endl;
+		std::cerr << RED << "\rError parsing request: missing first line element" << SET << std::endl;
 		return ;
 	}
 	request.setMethod(first.substr(0, end));
@@ -63,21 +63,21 @@ void			Parser::parseFirstLine(Request &request)
 	if (std::find(methods.begin(), methods.end(), request.getMethod()) == methods.end())
 	{
 		request.setRet(400);
-		std::cerr << RED << "Error parsing request: wrong method" << SET << std::endl;
+		std::cerr << RED << "\rError parsing request: wrong method" << SET << std::endl;
 		return ;
 	}
 
 	if ((start = first.find_first_not_of(' ', end))  == std::string::npos)
 	{
 		request.setRet(400);
-		std::cerr << RED << "Error parsing request: missing first line element" << SET << std::endl;
+		std::cerr << RED << "\rError parsing request: missing first line element" << SET << std::endl;
 		return ;
 	}
 
 	if ((end = first.find_first_of(' ', start)) == std::string::npos)
 	{
 		request.setRet(400);
-		std::cerr << RED << "Error parsing request: missing first line element" << SET << std::endl;
+		std::cerr << RED << "\rError parsing request: missing first line element" << SET << std::endl;
 		return ;
 	}
 	request.setPath(first.substr(start, end - start));
@@ -85,7 +85,7 @@ void			Parser::parseFirstLine(Request &request)
 	if ((end = first.find_first_not_of(' ', end)) == std::string::npos)
 	{
 		request.setRet(400);
-		std::cerr << RED << "Error parsing: missing first line element" << SET << std::endl;
+		std::cerr << RED << "\rError parsing: missing first line element" << SET << std::endl;
 		return ;
 	}
 
@@ -125,9 +125,6 @@ void			Parser::parseHeader(Request &request)
 	splitHeader(request);
 	parseFirstLine(request);
 
-	if (request.getCode() == 400)
-		return ;
-
 	for (stringVector::iterator i = request.header.begin() + 1; i != request.header.end(); i++)
 	{
 		token = findtoken(*i);
@@ -139,6 +136,7 @@ void			Parser::parseHeader(Request &request)
 		}
 	}
 	request.setNetwork(request.getHeader("Host"));
+
 }
 
 void			Parser::parseBody(Request& request)
